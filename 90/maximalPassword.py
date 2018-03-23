0509ebf61ed17694d9c17b482399f28532ab6b40
bakit <bakitds@gmail.com>
2018-03-23 11:23:43 +0600

    Updated lesson 90

--- 90/maximalPassword.py
+++ 90/maximalPassword.py
@@ -1,12 +1,26 @@
 def solution(S):
+    numbers = set([str(x) for x in range(10)])
+    chars = set([chr(x) for x in range(ord('A'), ord('Z'))] + [chr(x) for x in range(ord('a'), ord('z'))] + ['z', 'Z'])
+
     words = S.split()
-    maximal = 0
-    numbers = [x for x in range(0,10)]
+    maximal = ''
+
     for word in words:
-        n = 0
-        if len(word) % 2 != 0:
-            for symbol in word:
-                n += symbol in numbers
-        maximal = n if n % 2 == 0 and n > maximal else maximal
-                
-    return maximal
+        word_len = len(word)
+        if word_len % 2 == 0:
+            continue
+        nums = 0
+        chrs = 0
+        for letter in word:
+            if letter in numbers: nums += 1
+            elif letter in chars: chrs += 1
+            else: continue
+        if chrs % 2 == 0 and nums % 2 != 0 and chrs + nums == word_len and word_len > len(maximal):
+            maximal = word
+    return len(maximal) if len(maximal) else -1
+
+
+print(solution('0'), 1)
+print(solution('a'), -1)
+print(solution('zaq123edc'), 9)
+print(solution('test 5 a0A pass007 ?xy1'), 7)
